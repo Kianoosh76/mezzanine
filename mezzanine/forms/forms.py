3633333acab04ca195b2cfe5df119d3b55157d7f
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2018-04-06 14:59:33 +0430

    now all form fields are being store in self.form_fields

--- mezzanine/forms/forms.py
+++ mezzanine/forms/forms.py
@@ -130,7 +130,7 @@ class FormForForm(forms.ModelForm):
         instance and its related field model instances.
         """
         self.form = form
-        self.form_fields = form.fields.visible()
+        self.form_fields = form.fields.all()
         initial = kwargs.pop("initial", {})
         # If a FormEntry instance is given to edit, populate initial
         # with its field values.
